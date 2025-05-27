6cf0885ef98ad45d09a16ba39f8d23c53f2c94a2
Matthijs <matthijs.vannistelrooij@gmail.com>
2025-05-27 12:33:58 +0200

    update calendar

--- components/MyCalendar.tsx
+++ components/MyCalendar.tsx
@@ -162,7 +162,7 @@ export const MyCalendar = ({ events, setEvents }: MyCalendarProps) => {
     setOpen(true)
   }
   return (
-    <div className="p-4 rounded shadow">
+    <div className="p-5">
       <Dialog open={open} onOpenChange={setOpen}>
         <DialogContent className="bg-white">
           <DialogHeader>
