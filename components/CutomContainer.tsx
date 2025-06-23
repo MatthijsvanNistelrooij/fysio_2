01444025096b7b4ddd9323aa5d74c62dc5b456ae
Matthijs <matthijs.vannistelrooij@gmail.com>
2025-06-23 05:50:06 +0200

    update infocard

--- components/CutomContainer.tsx
+++ components/CutomContainer.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const CustomContainer = ({ children }: Props) => {
   return (
-    <div className="min-h-screen flex justify-center">
+    <div className="min-h-screen flex justify-center [bg-[#e9edf3]">
       <div className="w-full p-5 md:p-10">{children}</div>
     </div>
   )
