7b35ae9bcecbe930f6515e8cb1402daa6abeed34
Matthijs <matthijs.vannistelrooij@gmail.com>
2025-06-25 06:17:39 +0200

    new commit

--- app/(root)/dashboard/page.tsx
+++ app/(root)/dashboard/page.tsx
@@ -45,7 +45,7 @@ const Dashboard = () => {
 
   return (
     <CustomContainer>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         {pieData.map((item) => (
           <InfoCard key={item.name} title={item.name}>
             <p className="text-5xl font-semibold">
