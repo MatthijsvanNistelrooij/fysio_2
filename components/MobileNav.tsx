e3fb3eea85cc35c9629c393064f08ae72aa5ecdb
Matthijs <matthijs.vannistelrooij@gmail.com>
2025-06-26 00:44:34 +0200

    new commit

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -34,6 +34,7 @@ export const MobileNav = () => {
   ]
 
   const [darkmode, setDarkmode] = useAtom(darkmodeAtom)
+  const [signout, setSignout] = useState(false)
 
   return (
     <div
@@ -43,7 +44,7 @@ export const MobileNav = () => {
     >
       <div className="flex flex-row gap-2">
         <nav className="flex flex-row justify-between w-full">
-          <div className="mt-2 flex flex-row">
+          <div className="mt-2 flex flex-row border">
             {navItems.map((item) => {
               const isActive =
                 (item.url === "/clients" &&
@@ -79,64 +80,98 @@ export const MobileNav = () => {
               )
             })}
           </div>
-          <div className="flex">
-            <Settings2
-              onClick={() => setShowSettings(!showSettings)}
-              className="text-gray-500 mt-7 cursor-pointer select-none"
-              size={18}
-            />
-
-            {showSettings && (
-              <div
-                className={`absolute top-10 right-22 ${
-                  darkmode ? "bg-white" : "bg-gray-800"
-                } p-5 rounded-xl border`}
-              >
-                <div className="flex items-center space-x-2">
-                  <div className="flex gap-3 items-end mb-2 mr-10">
-                    {fontSizes.map(({ size, label, fontWeight }) => (
-                      <button
-                        key={size}
-                        onClick={() => setFontSize(size)}
-                        style={{
-                          fontSize: size,
-                          fontWeight: fontWeight,
-                        }}
-                        className={`cursor-pointer px-2 py-1 rounded transition ${
-                          fontSize === size
-                            ? "bg-gray-800 text-white border-b"
-                            : "bg-gray-200 text-gray-700 hover:bg-gray-300"
-                        }`}
-                        aria-label={`Set font size to ${size}`}
-                      >
-                        {label}
-                      </button>
-                    ))}
-                  </div>
-                  <button
-                    onClick={() => setDarkmode(!darkmode)}
-                    className={`w-12 h-6 rounded-full flex items-center px-1 transition-colors duration-300 cursor-pointer ${
-                      !darkmode ? "bg-gray-700" : "bg-gray-300"
-                    }`}
-                  >
-                    <div
-                      className={`w-4 h-4 bg-white rounded-full shadow-md transform transition-transform duration-300 ${
-                        !darkmode ? "translate-x-6" : "translate-x-0"
-                      }`}
-                    />
-                  </button>
-                </div>
-              </div>
-            )}
-
+          <div className="flex flex-row mt-2 gap-5 mr-5">
             <Link
-              href={"/"}
-              className="rounded-xl hover:text-gray-200 text-gray-600 hover:shadow font-bold transition mt-3"
-              onClick={handleSignOut}
+              href={""}
+              onClick={() => setShowSettings(!showSettings)}
+              className={`flex items-center gap-2 p-2 font-semibold transition
+                  ${
+                    darkmode
+                      ? "text-gray-400 hover:text-gray-800"
+                      : "text-gray-200"
+                  }
+                `}
             >
-              <LogOutIcon size={20} className="transform rotate-180 m-4" />
+              <Settings2 size={20} />
+            </Link>
+            <Link
+              href={""}
+              onClick={() => setSignout(!signout)}
+              className={`flex items-center gap-2 p-2 font-semibold transition
+                  ${
+                    darkmode
+                      ? "text-gray-400 hover:text-gray-800"
+                      : "text-gray-200"
+                  }
+                `}
+            >
+              <LogOutIcon size={20} className="transform rotate-180" />
             </Link>
           </div>
+          {showSettings && (
+            <div
+              className={`absolute top-18 right-32 ${
+                darkmode ? "bg-white" : "bg-gray-800"
+              } p-5 border`}
+            >
+              <div className="flex items-center space-x-2">
+                <div className="flex gap-3 items-end mb-2 mr-10">
+                  {fontSizes.map(({ size, label, fontWeight }) => (
+                    <button
+                      key={size}
+                      onClick={() => setFontSize(size)}
+                      style={{
+                        fontSize: size,
+                        fontWeight: fontWeight,
+                      }}
+                      className={`cursor-pointer px-2 py-1 rounded transition ${
+                        fontSize === size
+                          ? "bg-gray-800 text-white"
+                          : "bg-gray-200 text-gray-700 hover:bg-gray-300"
+                      }`}
+                      aria-label={`Set font size to ${size}`}
+                    >
+                      {label}
+                    </button>
+                  ))}
+                </div>
+                <button
+                  onClick={() => setDarkmode(!darkmode)}
+                  className={`w-12 h-6 rounded-full flex items-center px-1 transition-colors duration-300 cursor-pointer ${
+                    !darkmode ? "bg-gray-700" : "bg-gray-300"
+                  }`}
+                >
+                  <div
+                    className={`w-4 h-4 bg-white rounded-full shadow-md transform transition-transform duration-300 ${
+                      !darkmode ? "translate-x-6" : "translate-x-0"
+                    }`}
+                  />
+                </button>
+              </div>
+            </div>
+          )}
+          {signout && (
+            <div
+              className={`absolute top-18 right-5 ${
+                darkmode ? "bg-white text-gray-800" : "bg-gray-800"
+              } p-5 border`}
+            >
+              Signout?
+              <Link
+                href={"/"}
+                className={`rounded-xl hover:text-gray-200 text-gray-600 font-bold transition mt-3 ${
+                  darkmode ? "hover:text-gray-800" : ""
+                }`}
+                onClick={handleSignOut}
+              >
+                <LogOutIcon
+                  size={20}
+                  className="transform rotate-180 m-4"
+                  onClick={() => setSignout(!signout)}
+                />
+              </Link>
+            </div>
+          )}
         </nav>
       </div>
     </div>
