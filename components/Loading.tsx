7efc73d4d38f48436a5429d0ea5c1bcdd3da0912
Matthijs <matthijs.vannistelrooij@gmail.com>
2025-06-26 02:14:35 +0200

    new commit

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -3,7 +3,7 @@ import React from "react"
 const Loading = () => {
   return (
     <div className="flex flex-col gap-4 items-center justify-center h-96 text-gray-400 text-xl space-x-3">
-      <span>Loading clients...</span>
+      <span>Loading contacts...</span>
       <div className="w-6 h-6 border-2 border-blue-400 border-t-transparent rounded-full animate-spin"></div>
     </div>
   )
--- components/NoClients.tsx
+++ components/NoClients.tsx
@@ -5,7 +5,7 @@ const NoClients = () => {
   return (
     <div className="flex text-center items-center justify-center h-96 w-full">
       <div className="flex flex-col items-center p-10 gap-4 text-gray-500">
-        <p>No clients listed.</p>
+        <p>No contacts listed.</p>
         <Link
           className="bg-gray-900 hover:bg-gray-700 text-white px-4 py-2 text-sm rounded-md transition"
           href="/create"
