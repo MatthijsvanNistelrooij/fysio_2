bb3055d4e98faaadf83f9335c39965c99d7e2cb7
Matthijs <matthijs.vannistelrooij@gmail.com>
2025-07-22 06:06:49 +0200

    update theme

--- app/(root)/appointments/page.tsx
+++ app/(root)/appointments/page.tsx
@@ -14,7 +14,7 @@ const Appointments = () => {
 
   if (loading)
     return (
-      <div className="h-screen w-full flex justify-center items-center text-center">
+      <div className="h-screen text-white w-full flex justify-center items-center text-center">
         Loading...
       </div>
     )
