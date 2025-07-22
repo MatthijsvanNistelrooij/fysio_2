1ec66e77b7ba8d2de9736bc2fc99573f997ed96d
Matthijs <matthijs.vannistelrooij@gmail.com>
2025-07-22 06:02:09 +0200

    update theme

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,23 +1,16 @@
 "use client"
-import { darkmodeAtom } from "@/lib/store"
-import { useAtom } from "jotai"
+
 import Link from "next/link"
 
 export default function Home() {
-  const [darkmode] = useAtom(darkmodeAtom)
-
   return (
-    <div
-      className={` min-h-screen flex justify-center ${
-        darkmode ? "bg-[#e9edf3] text-gray-800" : "bg-gray-800 text-gray-200"
-      }  `}
-    >
-      <div className="p-5 w-full rounded-3xl m-5">
+    <div>
+      <div className="p-5 w-full rounded-3xl m-5 text-white">
         <div className="flex text-center items-center justify-center h-96 w-full">
           <div className="flex flex-col p-10 gap-3">
             Welkom terug!
             <Link
-              className={`border bg-gray-800 ${darkmode ? "border-gray-200" : "border-gray-500"} hover:bg-gray-700 rounded-md p-2 text-white `}
+              className={`bg-gray-800 hover:bg-gray-700 rounded-md p-2 text-white `}
               href="/create"
             >
               Create new Contact
--- components/Loading.tsx
+++ components/Loading.tsx
@@ -2,9 +2,9 @@ import React from "react"
 
 const Loading = () => {
   return (
-    <div className="flex flex-col gap-4 items-center justify-center h-96 text-gray-400 text-xl space-x-3">
+    <div className="flex flex-col gap-4 items-center justify-center h-96 text-white text-xl space-x-3">
       <span>Loading contacts...</span>
-      <div className="w-6 h-6 border-2 border-blue-400 border-t-transparent rounded-full animate-spin"></div>
+      <div className="w-6 h-6 border-2 border-white border-t-transparent rounded-full animate-spin"></div>
     </div>
   )
 }
